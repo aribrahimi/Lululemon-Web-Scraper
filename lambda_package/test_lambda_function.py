899753fae321f6ceede1484dde6ae979d616ce6c
Alireza <55057735+aribrahimi@users.noreply.github.com>
2024-04-18 02:11:23 +0000

    updated test case

--- lambda_package/test_lambda_function.py
+++ lambda_package/test_lambda_function.py
@@ -1,9 +1,13 @@
 # test_lambda_function.py
-import pytest
+import json
 from lambda_function import lambda_handler
 
 def test_lambda_handler():
-    response = lambda_handler({}, {}) 
+    response = lambda_handler({}, {})  
     assert response['statusCode'] == 200
-    assert 'Hello World' in response['body']
+    
+    # Parse the JSON response body
+    body = json.loads(response['body'])
 
+    # Assert that the response body includes a specific product name
+    assert any(product['productName'] == "Wunder Train High-Rise Tight 25\"" for product in body), "Product name not found in response"
